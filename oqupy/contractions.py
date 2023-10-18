FIXED BUG, few wires crossed in gradient.py recombination

oqupy/contractions.py
@@ -333,6 +333,16 @@ def compute_gradient_and_dynamics(
         # unpacks tuple, unlike calling function in usual forwardprop
         first_half_prop, second_half_prop = propagator_list[step]
         pt_mpos = _get_pt_mpos(process_tensors, step)
+        # if step == 1:
+        #     print(step)
+        #     print('process_tensor = ')
+
+        #     with np.printoptions(precision=4,suppress=True):
+        #         print(pt_mpos)
+        #     import sys
+        #     sys.exit()
+        # with np.printoptions(precision=4,suppress=True):
+        #     print(first_half_prop)
 
         current_node, current_edges = _apply_system_superoperator(
             current_node, current_edges, first_half_prop)
@@ -384,7 +394,7 @@ def compute_gradient_and_dynamics(
     current_edges = current_node[:]
 
     combined_deriv_list = []
-
+    # backprop_deriv_list = []
     if get_forward_and_backprop_list:
         backprop_deriv_list = [tn.replicate_nodes([current_node])[0]]
 

oqupy/gradient.py
@@ -191,11 +191,18 @@ def get_adjoint_derivs(
         # 0/False -> Pre node, 1/True -> Post node
         pre_post_bool = i % 2
         propagators_list_index = i // 2
+        pre_array,post_array = propagators[propagators_list_index]
+        # if first or last step, no extra propagaor needed so just set both to
+        # be the identity
+        if i == 0 or i == 2*len(propagators):
+            pre_array = np.identity(pre_array.shape[0])
+            post_array = np.identity(post_array.shape[0])
+
         deriv = _combine_derivs_without_propagator(
             target_deriv=gradient_dynamics.deriv_list[dtarget_index[i]],
             pre_post_decider=pre_post_bool,
-            pre=propagators[propagators_list_index][0],
-            post=propagators[propagators_list_index][1])
+            pre=pre_array,
+            post=post_array)
         derivs.append(deriv)
 
     return derivs
@@ -305,16 +312,21 @@ def _combine_derivs_without_propagator(
     # deriv is a post node -> extra node needed is a pre
     if pre_post_decider:
         extra_prop_node = tn.Node(pre)
-        target_deriv_node[0] ^ extra_prop_node[0]
+        target_deriv_node[1] ^ extra_prop_node[0]
 
     # deriv is a pre node -> extra node needed is a post
     else:
         extra_prop_node = tn.Node(post)
-        target_deriv_node[1] ^ extra_prop_node[0]
+        target_deriv_node[0] ^ extra_prop_node[1]
 
     final_node = target_deriv_node @ extra_prop_node
 
-    tensor = final_node.tensor
+    if pre_post_decider:
+        tensor = final_node.tensor
+    else:
+        # result is transposed dunno why
+        tensor = final_node.tensor.T
+
     return tensor
 
 def _combine_derivs(