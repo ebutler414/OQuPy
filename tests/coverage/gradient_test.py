9d44a125baa7d4e239b885edce58765d6e9b1f49
Eoin O'Neill <eoinoneill@Eoins-MacBook-Pro.local>
2024-02-20 10:12:08 +0000

    added gradients tests

--- tests/coverage/gradient_test.py
+++ tests/coverage/gradient_test.py
@@ -17,12 +17,83 @@ Tests for the oqupy.gradient module.
 
 import pytest
 import numpy as np
+from numpy import ndarray
 
-from oqupy.gradient import fidelity_gradient
+from typing import Callable,List,Tuple
+
+import oqupy
+import oqupy.operators as op
+
+from oqupy.gradient import state_gradient
 from oqupy.gradient import forward_backward_propagation
 
 def test_forward_backward_propagation():
-    assert True # ToDo
+    assert True
 
 def test_fidelity_gradient():
-    assert True # ToDo
+
+    dt = 0.2
+    num_steps = 20
+
+    # -- bath --
+    alpha = 0.08
+    omega_cutoff = 4.0
+    temperature = 1.6
+    pt_dkmax = 40
+    pt_epsrel = 1.0e-5
+
+    # -- initial and target state --
+    initial_state = op.spin_dm('down')
+    target_state = op.spin_dm('x-')
+
+    # -- initial parameter guess --
+    x0 = np.zeros(2*num_steps)
+    y0 = np.ones(2*num_steps) * (np.pi/2) / (dt*num_steps)
+    z0 = np.zeros(2*num_steps)
+
+    correlations = oqupy.PowerLawSD(
+    alpha=alpha,
+    zeta=1,
+    cutoff=omega_cutoff,
+    cutoff_type='exponential',
+    temperature=temperature)
+
+    bath = oqupy.Bath(0.5 * op.sigma('y'), correlations)
+    pt_tempo_parameters = oqupy.TempoParameters(
+        dt=dt,
+        epsrel=pt_epsrel,
+        dkmax=pt_dkmax)
+    process_tensor = oqupy.pt_tempo_compute(
+        bath=bath,
+        start_time=0.0,
+        end_time=num_steps * dt,
+        parameters=pt_tempo_parameters,
+        progress_type='bar')
+    
+    def hamiltonian(x, y, z):
+        h = np.zeros((2,2),dtype='complex128')
+        for var, var_name in zip([x,y,z], ['x', 'y', 'z']):
+            h += var * op.sigma(var_name)
+        return h
+    
+    parameterized_system = oqupy.ParameterizedSystem(hamiltonian)
+
+    gradient_dict = state_gradient(
+            system=parameterized_system,
+            initial_state=initial_state,
+            target_state=target_state,
+            process_tensor=process_tensor,
+            parameters=list(zip(x0,y0,z0)),
+            return_fidelity=True,
+            return_dynamics=True)
+    
+    dynamics = gradient_dict['dynamics']
+    derivatives = gradient_dict['gradprop']
+    
+    assert isinstance(dynamics,oqupy.Dynamics)
+    assert callable(hamiltonian)
+
+    assert len(derivatives) == process_tensor.__len__()
+    assert len(derivatives)+1 == len(dynamics)
+    
+
