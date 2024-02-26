84ca864ac52b374f0222979109edb92e1407c734
Eoin O'Neill <eoinoneill2506@outlook.com>
2024-02-26 22:48:12 +0000

    working optimisation code added

--- examples/fidelity-gradient.py
+++ examples/fidelity-gradient.py
@@ -18,8 +18,8 @@ from typing import List,Tuple
 # --- Parameters --------------------------------------------------------------
 
 # -- time steps --
-dt = 0.2
-num_steps = 20
+dt = 0.05
+num_steps = 200
 
 # -- bath --
 alpha = 0.08
@@ -30,12 +30,12 @@ pt_epsrel = 1.0e-5
 
 # -- initial and target state --
 initial_state = op.spin_dm('x-')
-target_state = op.spin_dm('z+')
+target_state = op.spin_dm('x+')
 
 # -- initial parameter guess --
+y0 = np.zeros(2*num_steps)
+z0 = np.ones(2*num_steps) * (np.pi) / (dt*num_steps)
 x0 = np.zeros(2*num_steps)
-y0 = np.ones(2*num_steps) * (np.pi/2) / (dt*num_steps)
-z0 = np.zeros(2*num_steps)
 
 parameter_list = list(zip(x0,y0,z0))
 
@@ -43,12 +43,12 @@ parameter_list = list(zip(x0,y0,z0))
 
 correlations = oqupy.PowerLawSD(
     alpha=alpha,
-    zeta=1,
+    zeta=3,
     cutoff=omega_cutoff,
-    cutoff_type='exponential',
+    cutoff_type='gaussian',
     temperature=temperature)
-bath = oqupy.Bath(0
-                   * op.sigma('y'), correlations)
+bath = oqupy.Bath(0.5
+                   * op.sigma('z'), correlations)
 pt_tempo_parameters = oqupy.TempoParameters(
     dt=dt,
     epsrel=pt_epsrel,
@@ -65,7 +65,7 @@ process_tensor = oqupy.pt_tempo_compute(
 def hamiltonian(x, y, z):
     h = np.zeros((2,2),dtype='complex128')
     for var, var_name in zip([x,y,z], ['x', 'y', 'z']):
-        h += var * op.sigma(var_name)
+        h += 0.5* var * op.sigma(var_name)
     return h
 
 parametrized_system = oqupy.ParameterizedSystem(hamiltonian)
@@ -85,10 +85,17 @@ fidelity_dict = state_gradient(
 
 print(f"the fidelity is {fidelity_dict['fidelity']}")
 print(f"the fidelity gradient is {fidelity_dict['gradient']}")
-t, s_x = fidelity_dict['dynamics'].expectations(op.sigma("x"), label='x')
-t, s_y = fidelity_dict['dynamics'].expectations(op.sigma("x"))
-t, s_z = fidelity_dict['dynamics'].expectations(op.sigma("x"))
-plt.plot(t,s_x)
+t, s_x = fidelity_dict['dynamics'].expectations(op.sigma("x"))
+t, s_y = fidelity_dict['dynamics'].expectations(op.sigma("y"))
+t, s_z = fidelity_dict['dynamics'].expectations(op.sigma("z"))
+
+plt.title("Pre-optimisation evolution of components")
+
+plt.plot(t,s_x,label='x')
+plt.plot(t,s_y,label='y')
+plt.plot(t,s_z,label='z')
+
+plt.legend()
 
 # ----------- Optimisation of control parameters w.r.t. infidelity ---------------
 
@@ -108,15 +115,16 @@ def unflatten_list(flat_list):
     return parameter_list
 
 def sum_adjacent_elements(list:List)->List:
-    # maybe this goes in helpers.py or utils.py?
+
     half_the_size = len(list) / 2
     assert (half_the_size).is_integer(), \
         'if one output from both pre and post node is given, result must be even'
     half_the_size = int(half_the_size)
 
-    # https://stackoverflow.com/a/29392016
+
     summed_array = np.reshape(list,(half_the_size,2)).sum(axis=1)
     return summed_array
+
 
 def infidelity(parameter_list_flat):
 
@@ -132,6 +140,7 @@ def infidelity(parameter_list_flat):
     
     return -float(return_dict['fidelity'])
 
+# infidelity gradient to point optimiser in the right direction (note: minimising infidelity therefore sign of jacobian flipped)
 def fidelity_jacobian(parameter_list_flat):
 
     parameter_list_var = unflatten_list(parameter_list_flat)
@@ -144,14 +153,10 @@ def fidelity_jacobian(parameter_list_flat):
         parameters=parameter_list_var)
     
     fidelity_jacobian = flatten_list(fidelity_dict['gradient'])
-
-    print(len(fidelity_jacobian))
-
+ 
     fidelity_jacobian = sum_adjacent_elements(fidelity_jacobian)
 
-    print(len(fidelity_jacobian))
-
-    piecewiseconst_jacobian = [0]*120
+    piecewiseconst_jacobian = [0]*2*3*num_steps
 
     for i,element in enumerate(fidelity_jacobian):
         piecewiseconst_jacobian[2*i] = fidelity_jacobian[i]
@@ -159,10 +164,10 @@ def fidelity_jacobian(parameter_list_flat):
 
     fort_jac =np.asfortranarray(piecewiseconst_jacobian)
 
-    return fort_jac
+    return -fort_jac
 
-lower_bound = -np.pi 
-upper_bound = np.pi 
+lower_bound = -5
+upper_bound = 5
 
 bounds = Bounds(lb=lower_bound,ub=upper_bound)
 
@@ -182,11 +187,7 @@ print("The jacobian was found to be : ",optimization_result.jac)
 
 optimized_parameters_flat = optimization_result.x
 
-
-print(max(abs(optimized_parameters_flat)))
-print(min(abs(optimized_parameters_flat)))
-
-times = np.arange(0,40)
+times = np.arange(0,2*num_steps)
 optimized_parameters = unflatten_list(optimized_parameters_flat)
 
 optimized_dynamics = state_gradient(
@@ -199,6 +200,7 @@ optimized_dynamics = state_gradient(
         return_dynamics=True)
 
 fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1) 
+fig.suptitle("Optimisation results")
 
 optimized_x = np.array([parameter[0] for parameter in optimized_parameters])
 optimized_y = np.array([parameter[1] for parameter in optimized_parameters])
@@ -221,9 +223,11 @@ fidelity=[]
 for state in dynamics.states:
     sqrt_final_state =sqrtm(state)
     intermediate_1 = sqrt_final_state @ target_state
+
     inside_of_sqrt = intermediate_1 @ sqrt_final_state
     fidelity.append((sqrtm(inside_of_sqrt).trace())**2)
-times = np.arange(0,21)
+
+times = np.arange(0,201)
 
 ax2.plot(times,fidelity)
 
