b7564b315ade6a5d36f5a631275c7cfc3bc2911b
Eoin O'Neill <eoinoneill@Eoins-MBP.station>
2024-04-09 01:04:43 +0100

    rewrite create_delta in util.py

---
+++ tests/performance/pt_gradient.py
@@ -0,0 +1,15 @@
+import sys
+sys.path.insert(0,'.')
+from IPython import embed
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import oqupy
+import oqupy.operators as op
+
+from typing import List,Tuple
+
+# ----- Performance test for adjoint method -----
+
+
